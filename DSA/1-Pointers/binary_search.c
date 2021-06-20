84fdb4180b91cffcd6022ddaa2d45fd1ac544d83
kinivasu <vasudevkinit2001@gmail.com>
2021-06-20 15:45:58 +0530

    fx

--- DSA/1-Pointers/binary_search.c
+++ DSA/1-Pointers/binary_search.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 int BinarySearch(int list[], int *endPtr, int target, int **locPtr);
 
-int main(){
+int main()
+{
 
-    int a[]={10,20,30,40,50};
-    int n=5;
+    int a[] = {10, 20, 30, 40, 50};
+    int n = 5;
     int t;
     int *loc; //Pointer to hold the address of element, if found
     int target;
 
-    int *endPtr = a + n-1;
+    int *endPtr = a + n - 1;
     printf("Array Elements :\n");
-    for(int *p=a;p<=endPtr;p++)
-        printf("%p = %d\n",p,*p);
+    for (int *p = a; p <= endPtr; p++)
+        printf("%p = %d\n", p, *p);
 
     printf("Enter value to be searched :");
-    scanf("%d",&target);
+    scanf("%d", &target);
     //function call
     /*Address of the pointer variable, loc is sent to function
     changes to this is reflected back in the main function
     */
     int flag = BinarySearch(a, endPtr, target, &loc);
     if (flag)
-        printf("Element found at %p",loc);
+        printf("Element found at %p\n", loc);
     else
-        printf("Element Not Found");
+        printf("Element Not Found\n");
 }
 
 /*
@@ -33,32 +34,31 @@ loc from main and returns address of element in loc, if found
 
 locPtr --->loc---> searching element in array
 */
-int BinarySearch(int list[], int *endPtr, int target, int **locPtr){
+int BinarySearch(int list[], int *endPtr, int target, int **locPtr)
+{
     int *firstPtr = list;
     int *lastPtr = endPtr;
     int *midPtr;
     int no_of_elements, midIndex;
-    while(firstPtr<=lastPtr){
+    while (firstPtr <= lastPtr)
+    {
         //find the number of elements between last and first pointers
         //and find the index of mid element
-        no_of_elements = (lastPtr - firstPtr+1);
-        midIndex = no_of_elements/2;
+        no_of_elements = (lastPtr - firstPtr + 1);
+        midIndex = no_of_elements / 2;
         midPtr = firstPtr + midIndex;
-
 
         printf("\n%p= %d %p= %d No of elements= %d midindex= %d\n ", lastPtr, *lastPtr, firstPtr, *firstPtr, no_of_elements, midIndex);
 
-        printf("Mid Element Address = %p Element = %d\n",midPtr, *midPtr);
+        printf("Mid Element Address = %p Element = %d\n", midPtr, *midPtr);
         if (target > *midPtr)
             firstPtr = midPtr + 1;
+        else if (target < *midPtr)
+            lastPtr = midPtr - 1;
         else
-            if (target< *midPtr)
-                lastPtr = midPtr - 1;
-            else
-                break;
+            break;
     }
     *locPtr = midPtr;
 
     return (*midPtr == target);
 }
-
--- DSA/2-Structs,Recursion,Typedefs/towers.c
+++ DSA/2-Structs,Recursion,Typedefs/towers.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-int tower (int n, char  source, char temp, char destination)
-{   static int Count=0;
-	if(n==1)
+int tower(int n, char source, char temp, char destination)
+{
+	static int Count = 0;
+	if (n == 1)
 	{
-        Count++;
-        printf("move disk 1 from %c to %c\n",source,destination);
-        return Count;
+		Count++;
+		printf("move disk 1 from %c to %c\n", source, destination);
+		return Count;
 	}
 
 	/*moving n-1 disks from A to B using C as auxiliary*/
-	tower(n-1, source, destination, temp);
-    Count++;
-	printf("move disk %d from %c to %c\n",n,source,destination);
+	tower(n - 1, source, destination, temp);
+	Count++;
+	printf("move disk %d from %c to %c\n", n, source, destination);
 
 	/*moving n-1 disks from B to C using A as auxiliary*/
-	tower(n-1, temp, source, destination);
+	tower(n - 1, temp, source, destination);
 }
 
-int main(){
+int main()
+{
 
-   int x = tower(4,'A', 'B','C');
-   printf("No. of moves = %d",x);
+	// Source, Temp(Auxillary), Destination
+	int x = tower(4, 'A', 'B', 'C');
+	printf("No. of moves = %d", x);
 }
